ba1d65ab96661a68773ccc0e70b18f6301516497
密码111111 <23580151@qq.com>
2018-08-23 21:43:36 +0800

    new windows

---
+++ app/src/main/java/com/example/a111111/diancan/gld/inside/cdlist.java
@@ -0,0 +1,67 @@
+package com.example.a111111.diancan.gld.inside;
+
+public class cdlist {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    private String name;
+    private String price;
+    private String desc;
+    private String img;
+
+
+
+    public cdlist() {
+    }
+
+    public cdlist(String id,String name, String price, String desc,String img) {
+        this.name = name;
+        this.img = img;
+        this.price = price;
+        this.id = id;
+        this.desc = desc;
+    }
+
+
+
+
+}
